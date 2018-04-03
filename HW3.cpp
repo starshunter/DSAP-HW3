46a8beee260c6749505fe41ede68341a1b6cdcc4
Starshunter <petercheng0303@gmail.com>
2018-04-03 11:31:43 +0800

    1-b
    1-b

--- HW3.cpp
+++ HW3.cpp
@@ -1,70 +1,46 @@
 #include <iostream>
 using namespace std;
 
-class player
+class instrument
 {
 protected:
-	int number;
-	string position;
+	int risk;
+	int premium;
+	int quantity;
+};
+class stock:public instrument
+{
+
+};
+class bond:public instrument
+{
+
+};
+class fund:public instrument
+{
+
+};
+class insurance:public instrument
+{
+
+};
+class customer
+{
+protected:
+	int cnt;
+	int capacity;
 	string name;
-	int height;
-	int weight;
-public:
-	virtual void print()=0;
-};
-class pitcher:public player
-{
-private:
-	double ERA;
-public:
-	void print()
-	{
-		cout<<name<<" "<<height<<" "<<weight<<" "<<number<<endl;
-		cout<<ERA<<endl;
-	}
-};
-class catcher:public player
-{
-private:
-	double CS;
-	double FTCP;
-public:
-	void print()
-	{
-		cout<<name<<" "<<height<<" "<<weight<<" "<<number<<endl;
-		cout<<CS<<" "<<FTCP<<endl;
-	}
-};
-class infielder:public player
-{
-private:
-	double FTCP;
-public:
-	void print()
-	{
-		cout<<name<<" "<<height<<" "<<weight<<" "<<number<<endl;
-		cout<<FTCP<<endl;
-	}
-};
-class outfileder:public player
-{
-	double FTCP;
-public:
-	void print()
-	{
-		cout<<name<<" "<<height<<" "<<weight<<" "<<number<<endl;
-		cout<<FTCP<<endl;
-	}
-};
-class team
-{
-private:
-	player *member[1];
-public:
-	void print();
-};
-void team::print()
-{
-	for(int i=0;i<9;i++)
-		member[i]->print();
+	instrument **instrumentPtr;
 }
+class superVIP:public customer
+{
+
+};
+class VIP:public customer
+{
+
+};
+class normal:public customer
+{
+
+};
