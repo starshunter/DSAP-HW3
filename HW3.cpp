52d64543f2f59e609eaa1c00fb157781d7f0826b
Starshunter <petercheng0303@gmail.com>
2018-04-03 11:13:31 +0800

    2-a
    2-a

--- HW3.cpp
+++ HW3.cpp
@@ -6,30 +6,65 @@ class player
 protected:
 	int number;
 	string position;
+	string name;
+	int height;
+	int weight;
+public:
+	virtual void print()=0;
 };
-
 class pitcher:public player
 {
-
+private:
+	double ERA;
+public:
+	void print()
+	{
+		cout<<name<<" "<<height<<" "<<weight<<" "<<number<<endl;
+		cout<<ERA<<endl;
+	}
 };
-
 class catcher:public player
 {
-
+private:
+	double CS;
+	double FTCP;
+public:
+	void print()
+	{
+		cout<<name<<" "<<height<<" "<<weight<<" "<<number<<endl;
+		cout<<CS<<" "<<FTCP<<endl;
+	}
 };
-
 class infielder:public player
 {
-
+private:
+	double FTCP;
+public:
+	void print()
+	{
+		cout<<name<<" "<<height<<" "<<weight<<" "<<number<<endl;
+		cout<<FTCP<<endl;
+	}
 };
-
 class outfileder:public player
 {
-
+	double FTCP;
+public:
+	void print()
+	{
+		cout<<name<<" "<<height<<" "<<weight<<" "<<number<<endl;
+		cout<<FTCP<<endl;
+	}
 };
-
 class team
 {
 private:
-	player *member[9];
+	player *member[1];
+public:
+	void print();
 };
+void team::print()
+{
+	for(int i=0;i<9;i++)
+		member[i]->print();
+}
