6c21b3a992e8ebf534e7155f713ed296d4123eed
Starshunter <petercheng0303@gmail.com>
2018-04-07 17:30:06 +0800

    4 function
    4 function

--- HW3.cpp
+++ HW3.cpp
@@ -9,29 +9,17 @@ struct MyException: public runtime_error
     MyException(const string &s):runtime_error(s+" is not a valid password"){}
 };
 
-
+void create_account();
 
 int main()
 {
-	string username,password;
-	cout<<"Enter your username"<<endl;
-	cin>>username;
-	bool valid=false;
-	while(!valid)
+	bool success=false;
+	while(!success)
 	{
-		cout<<"Enter your password (It must contain at least one number and longer than 8)"<<endl;
 		try
 		{
-			cin>>password;
-			bool a=false;
-			if(password.length()<8)
-				throw(MyException(password));
-			for(int i=0;i<password.length();i++)
-				if(password[i]-48>=0&&password[i]-48<=9)
-					a=true;
-			if(!a)
-				throw(MyException(password));
-			valid=true;
+			create_account();
+			success=true;
 		}
 		catch(MyException &e)
 		{
@@ -39,4 +27,22 @@ int main()
 		}
 	}
 	return 0;
+}
+
+void create_account()
+{
+	string username,password;
+	cout<<"Enter your username"<<endl;
+	cin>>username;
+	cout<<"Enter your password (It must contain at least one number and longer than 8)"<<endl;
+	cin>>password;
+	bool a=false;
+	if(password.length()<8)
+		throw(MyException(password));
+	for(int i=0;i<password.length();i++)
+		if(password[i]-48>=0&&password[i]-48<=9)
+			a=true;
+	if(!a)
+		throw(MyException(password));
+	
 }
